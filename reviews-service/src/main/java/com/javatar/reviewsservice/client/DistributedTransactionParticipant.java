759544d9400bbf57a46166cc3e75d22d9068499d
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2024-11-27 09:23:55 -0800

    Refactoring reviews-service

---
+++ reviews-service/src/main/java/com/javatar/reviewsservice/client/DistributedTransactionParticipant.java
@@ -0,0 +1,18 @@
+package com.javatar.reviewsservice.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DistributedTransactionParticipant {
+
+    private String serviceId;
+
+    private DistributedTransactionStatus status;
+
+}
---
+++ reviews-service/src/main/java/com/javatar/reviewsservice/client/DistributedTransactionStatus.java
@@ -0,0 +1,5 @@
+package com.javatar.reviewsservice.client;
+
+public enum DistributedTransactionStatus {
+    NEW, CONFIRMED, ROLLBACK, TO_ROLLBACK
+}
